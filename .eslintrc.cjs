2110535468581547c5863e1af30708bb2d5f622c
Kevin <k.bardoux@orange.fr>
2025-08-12 14:10:02 +0200

    init

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,25 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { 
+    browser: true, 
+    es2020: true,
+    node: true
+  },
   extends: [
     'eslint:recommended',
-    '@typescript-eslint/recommended',
-    '@typescript-eslint/recommended-requiring-type-checking',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'node_modules'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'no-unused-vars': 'warn',
+    'no-console': 'warn',
   },
 }
